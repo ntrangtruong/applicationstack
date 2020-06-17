47608bd5292fcdc16519e23be4cac6e01a8af3d1
stef2nn <61807101+stef2nn@users.noreply.github.com>
2020-06-16 20:15:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('<h2>Hello World !</h2>');
 });
-
-const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log(`Example app listening at http://${host}:${port}`);
+app.listen(3000, function () {
+  console.log('Server listening on port 3000!');
 });
